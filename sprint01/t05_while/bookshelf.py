2a44232398f2eedee6942ec6c120b7a1b6fe71bc
nnocturnnn <vikchehovich@gmail.com>
2021-04-05 22:37:14 +0300

    fix 5

--- sprint01/t05_while/bookshelf.py
+++ sprint01/t05_while/bookshelf.py
@@ -3,9 +3,9 @@
 def add_to_bookshelf(book_to_add, bookshelf):
     i = 0
     while i < len(bookshelf):
-        if i == "---":
-            i = book_to_add
+        if bookshelf[i] == "---":
+            bookshelf[i] = book_to_add
             return True
-        else:
-            return False
+        i += 1
+    return False
             
