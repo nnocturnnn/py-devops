81b23571f630dc1310cae39705ddb1d9e6cff764
nnocturnnn <vikchehovich@gmail.com>
2021-04-14 18:41:54 +0300

    regex

---
+++ DevOps/sprint02/t05_regex/regex.py
@@ -0,0 +1,13 @@
+import re
+
+
+
+def check_address(arg_dict):
+    tpl = r'^Ukraine,[ ]*[A-Za-z-[ ]*]*,[ ]*[A-Za-z-[ ]*]*[ ]*\d{1,6},[ ]*\d{5}$'
+    r_list = []
+    for key in arg_dict:
+        if re.match(tpl, arg_dict[key]) is not None:
+            r_list.append(f"The address of {key} is valid.")
+        else:
+            r_list.append(f"The address of {key} is invalid.")
+    return r_list
