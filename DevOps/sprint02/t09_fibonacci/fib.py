78d7f1b904e8b3598d6c0d24217efb57f6963dd0
nnocturnnn <vikchehovich@gmail.com>
2021-04-15 13:46:41 +0300

    all

---
+++ DevOps/sprint02/t09_fibonacci/fib.py
@@ -0,0 +1,30 @@
+
+
+def fib(nterms):
+    n1, n2 = 0, 1
+    count = 0
+
+    if nterms == 1:
+        return n1
+    else:
+        while count < nterms:
+            nth = n1 + n2
+            n1 = n2
+            n2 = nth
+            count += 1
+        return n1
+
+
+def fib_generator(nterms):
+    n1, n2 = 0, 1
+    count = 0
+
+    if nterms == 1:
+        yield n1
+    else:
+        while count < nterms:
+            yield n1
+            nth = n1 + n2
+            n1 = n2
+            n2 = nth
+            count += 1
