cafc93257fab600a2d7f224040c7a790869782dd
nnocturnnn <vikchehovich@gmail.com>
2021-04-28 00:17:31 +0300

    t01 lamb

--- DevOps/sprint03/t01_clear_words/clear_words.py
+++ DevOps/sprint03/t01_clear_words/clear_words.py
@@ -1,14 +1,4 @@
-
 import re
 
-def clear_words(arg_str):
-    string = re.split('? |! |. |: |; |, |-', arg_str)
-    print(string)
-    # l = list(map(lambda x: x, re.split('? |! |. |: |; |, |-', arg_str)))
-    # print(l)
-
-
-text_example_1 = 'WOMAN: Yes?, ENCYCLOPEDIA SALESMAN: Burglar, madam. WOMAN: '\
-                 'Are you an encyclopaedia salesman?'
-
-clear_words(text_example_1)
+def clear_words(s):
+    return list(filter(None,list(map(str.strip, re.split(r" |\?|!|\.|:|;|,|-",s)))))
