006f751d136a0d9d755be4c5cdfc08c10914b3c5
nnnoc <vikchehovich@gmail.com>
2021-05-07 22:22:39 +0300

    rm

--- DevOps/sprint03-old/t02_print_a_dictionary/printer.py
+++ DevOps/sprint03-old/t02_print_a_dictionary/printer.py
@@ -4,5 +4,5 @@
 def dictionary_printer(arg_dict):
     if not isinstance(arg_dict, dict):
         raise ValueError("The argument must be a dict.")
-    return list(map(lambda d : str(d[0]) + ' - ' + str(d[1]) ,arg_dict.items()))
+    return list(map(lambda d : str(d[0]) + ' - ' + str(d[1]), arg_dict.items()))
 
