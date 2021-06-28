ac31a64304b3264fd3cb2427705342f8734a07c2
nnnoc <vikchehovich@gmail.com>
2021-06-28 11:11:37 +0300

    fix

--- DevOps/sprint03/t03_read_file/read_file.py
+++ DevOps/sprint03/t03_read_file/read_file.py
@@ -5,7 +5,7 @@ def read_file(arg_str):
     try:
         with open(arg_str, "r") as file_handler:
             var = file_handler.read()
-            print(f"File {arg_str} has the following message:\n{var}")
+            print(f'File "{arg_str}" has the following message:\n{var}')
     except :
-        print(f"Failed to read file {arg_str}.")
+        print(f'Failed to read file "{arg_str}".')
     
